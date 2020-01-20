9520689672973a9007711f8ca434baad099cf0b2
zerihunz <zerihun.tegegn@cgifederal.com>
2020-01-20 11:40:14 -0500

    Operators and Variables

---
+++ operators.py
@@ -0,0 +1,20 @@
+a = 3 + 5
+print(a)
+b = 'some' + 'bar'
+print(b)
+c = -5.2
+print(c)
+d = 'la' * 5
+print(d)
+#Power operator is **
+e = 2 ** 6
+print(e)
+#Division operator is /, Divide and floor operator is //, what that means is divide x by y and round the answer to the nearest integer, if one of the values is float, the result is a float
+#Prints 4
+f = 13 // 3
+print(f)
+#Prints -5
+f = -13 // 3
+print(f)
+f = 9 // 1.81
+print(f)
---
+++ variables.py
@@ -0,0 +1,8 @@
+i=5
+print(i)
+i = i + 1
+print(i)
+
+i = '''This is a multi-line string.
+This is the second line.'''
+print(i)
